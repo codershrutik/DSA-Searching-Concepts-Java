4f48c27b8a1e57bd82170b8f9a62edb2e807891d
codershrutik <72141758+codershrutik@users.noreply.github.com>
2024-06-01 04:30:09 +0000

    Searching operations on 2D arrays

---
+++ linear-search/maxTwoD.java
@@ -0,0 +1,30 @@
+public class maxTwoD {
+    public static void main(String[] args) {
+        int[][] arr = {
+            {23,4,1},
+            {18,12,3,9},
+            {78,99,34,56},
+            {18,12}
+        };
+        int ans = max(arr);
+        System.out.println(ans);
+
+    }
+
+    static int max(int[][] arr){
+        // if(arr.length == 0){
+        //     return -1;
+        // }
+        int max = Integer.MIN_VALUE;
+
+        for(int i = 0;i<arr.length;i++){
+            for(int j = 0;j<arr[i].length;j++){
+                if(arr[i][j]>max){
+                    max = arr[i][j];
+                }
+            }
+        }
+        
+        return max;
+    }
+}
---
+++ linear-search/searchInTwoD.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+public class searchInTwoD {
+    public static void main(String[] args) {
+        int[][] arr = {
+            {23,4,1},
+            {18,12,3,9},
+            {78,99,34,56},
+            {18,12}
+        };
+
+        int target = 99;
+        int[] ans = search(arr, target);
+        System.out.println(Arrays.toString(ans));
+    }
+
+    static int[] search(int[][] arr, int target){
+        if(arr.length == 0){
+            return new int[]{-1,-1};
+        }
+
+        for(int i = 0; i<arr.length;i++){
+            for(int j = 0;j<arr[i].length;j++){
+                if(arr[i][j] == target){
+                    return new int[]{i,j};
+                }
+            }
+        }
+
+        return new int[]{-1,-1};
+        // return -1;
+    }
+
+
+}
