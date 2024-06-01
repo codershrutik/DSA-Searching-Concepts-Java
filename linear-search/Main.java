cd07b2c353421c6848bf93501f07a0c0690e6f90
codershrutik <72141758+codershrutik@users.noreply.github.com>
2024-06-01 03:29:31 +0000

    Added basic linear search program

---
+++ linear-search/Main.java
@@ -0,0 +1,25 @@
+public class Main{
+    public static void main(String[] args){
+        int[] nums = {23, 45, 1,2,8,19,-3,16,-11,28};
+        int target = 19;
+
+        int ans = linearSearch(nums, target);
+        System.out.println(ans);
+        
+    }
+
+    //searching in the array - returning the index if the item is found, else return -1
+    static int linearSearch(int[] arr, int target){
+        if(arr.length == 0){
+            return -1;
+        }
+
+        for(int i=0; i<arr.length;i++){
+            if(arr[i] == target){
+                return i;
+            } else{
+                return -1;
+            }
+        }
+    }
+}
